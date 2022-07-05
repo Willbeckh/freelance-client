ac9cef937e2d5f522eaf363a085f9e8e688e0fb8
Jeff <Xavierjeff451@gmail.com>
2022-07-05 15:37:11 +0300

    register form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -8,7 +8,7 @@ import { FormBuilder } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
