7fb75f5fdccaaf7090e836afa986a962339c1da5
Jeff <Xavierjeff451@gmail.com>
2022-07-06 10:05:00 +0300

    login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(private formBuilder:FormBuilder,private http:HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email:'',
+      password:''
+    })
+  }
+  submit():void{
+    console.log(this.form.getRawValue())
+    this.http.post('http://127.0.0.1:8000/login/',this.form.getRawValue(),{withCredentials: true} )
+    .subscribe(()=>this.router.navigate(['/']))
+
   }
 
 }
