e0fcda5c331bda101f91bf0db73a48db580ee94e
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-14 05:31:58 +0300

    ch: create interface for job object

---
+++ src/app/job.ts
@@ -0,0 +1,10 @@
+export class Job {
+  // class representation for job post
+  name: string;
+  company: string;
+  location: string;
+  host: number;
+  description: string;
+  experience: string;
+  status: string;
+}
