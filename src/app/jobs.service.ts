15f61160a982277736415f78e69d4ba6d820cddc
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-14 05:38:41 +0300

    ft: create httpclient for getting jobs

--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {  Injectable } from '@angular/core';
+import { Observable, retry, throwError } from 'rxjs';
+import { Job } from './job';
+import { catchError } from 'rxjs/operators';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JobsService {
+  baseurl = 'localhost:8000/';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // api http headers
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    }),
+  };
+
+
+  // http methods
+// get all jobs
+  getJobs(): Observable<any> {
+    return this.http.get<any>(this.baseurl + 'jobs/', this.httpOptions)
+    .pipe(retry(1), catchError(this.errorHandler));
+  };
+
+
+  // error handling
+  errorHandler(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError (() => {
+      return errorMessage;
+    });
+  }
 }
