b0212bf29fec3bd9e0aa7b59a0f763304a074b16
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-14 05:36:46 +0300

    pt: add route for landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { AuthComponent } from './auth/auth.component';
+import { JobsComponent } from './jobs/jobs.component';
 
 
 const routes: Routes = [
-  {path:'profile',component:AccountComponent},
-  {path:'auth',component:AuthComponent},
+  { path: 'profile', component: AccountComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: JobsComponent },
 ];
 
 @NgModule({
