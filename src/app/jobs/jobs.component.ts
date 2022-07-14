bc86791d57f775a45e33d5d305de639da89f154d
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-15 00:56:04 +0300

    st: add icons, adn style job cards

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,7 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {
+  faBuilding,
+  faClockFour,
+  faFireFlameCurved,
+  faLocationPin,
+  faStairs,
+} from '@fortawesome/free-solid-svg-icons';
 import { Job } from '../job';
 import { JobsService } from '../jobs.service';
+
 
 @Component({
   selector: 'app-jobs',
@@ -9,20 +17,25 @@ import { JobsService } from '../jobs.service';
   styleUrls: ['./jobs.component.css'],
 })
 export class JobsComponent implements OnInit {
-  jobs: any;
   imageSrc = 'assets/images/dev.png';
-
+  faLocationPin = faLocationPin;
+  faBuilding = faBuilding;
+  faStairs = faStairs;
+  faFireFlameCurved = faFireFlameCurved;
+  faClockFour = faClockFour;
   constructor(public jobService: JobsService, private http: HttpClient) {}
 
+  obj: any;
+
   ngOnInit(): void {
-    this.jobs = this.getAllJobs();
+    this.obj = this.getAllJobs();
   }
 
   // get all available jobs
   getAllJobs() {
-    console.log('jobs>>', this.jobs);
+    console.log('jobs>>', this.obj);
     this.jobService.getJobs().subscribe((data) => {
-      this.jobs = data;
+      this.obj = data;
     });
   }
 }
