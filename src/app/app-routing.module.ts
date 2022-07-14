6b07c9bf7a2d0867aedb297fa2359bb5276a6e13
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-15 01:24:38 +0300

    ft: add route for single job component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { AuthComponent } from './auth/auth.component';
 import { JobsComponent } from './jobs/jobs.component';
+import { OnejobComponent } from './onejob/onejob.component';
 
 
 const routes: Routes = [
+  { path: '', component: JobsComponent },
   { path: 'profile', component: AccountComponent },
   { path: 'auth', component: AuthComponent },
-  { path: '', component: JobsComponent },
+  { path: 'job', component: OnejobComponent },
 ];
 
 @NgModule({
