1e50dd37e933268c1d4801aaa2637a4c32d4374b
Willbeckh <wileyhndirangu04@gmail.com>
2022-07-14 06:15:41 +0300

    fx: update api base url

--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {  Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable, retry, throwError } from 'rxjs';
 import { Job } from './job';
 import { catchError } from 'rxjs/operators';
@@ -7,37 +7,36 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class JobsService {
-  baseurl = 'localhost:8000/';
+  baseurl = 'http://localhost:8000/';
   constructor(private http: HttpClient) {}
 
   // api http headers
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      // Accept: '*/*',
     }),
   };
 
-
   // http methods
-// get all jobs
+  // get all jobs
   getJobs(): Observable<any> {
-    return this.http.get<any>(this.baseurl + 'jobs/', this.httpOptions)
-    .pipe(retry(1), catchError(this.errorHandler));
-  };
-
+    return this.http
+      .get<any>(this.baseurl + 'jobs/', this.httpOptions)
+      .pipe(retry(1), catchError(this.errorHandler));
+  }
 
   // error handling
   errorHandler(error) {
     let errorMessage = '';
-    if(error.error instanceof ErrorEvent) {
+    if (error.error instanceof ErrorEvent) {
       // Get client-side error
       errorMessage = error.error.message;
     } else {
       // get server-side error
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-    return throwError (() => {
+    return throwError(() => {
       return errorMessage;
     });
   }
